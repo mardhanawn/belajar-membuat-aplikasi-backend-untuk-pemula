2afa7aa8e269e78dc9c8b55ef2ddf41ab6f3d3a1
ardhanaawahyu <ardhanaawahyu@bee.id>
2023-05-03 18:42:01 +0700

    Routing Request

--- server.js
+++ server.js
@@ -4,25 +4,39 @@ const requestListener = (request, response) => {
   response.setHeader("Content-Type", "text/html");
   response.statusCode = 200;
 
-  const { method } = request;
+  const { method, url } = request;
 
-  if (method === "GET") {
-    response.end("<h1>Hello!</h1>");
-  }
+  if (url === "/") {
+    if (method === "GET") {
+      response.end("<h1>Ini adalah homepage</h1>");
+    } else {
+      response.end(
+        `<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`
+      );
+    }
+  } else if (url === "/about") {
+    if (method === "GET") {
+      response.end("<h1>Halo! Ini adalah halaman about</h1>");
+    } else if (method === "POST") {
+      let body = [];
 
-  if (method === "POST") {
-    let body = [];
+      request.on("data", (chunk) => {
+        console.log("chunk", chunk);
+        body.push(chunk);
+      });
 
-    request.on("data", (chunk) => {
-      console.log("chunk", chunk);
-      body.push(chunk);
-    });
-
-    request.on("end", () => {
-      body = Buffer.concat(body).toString();
-      const { name } = JSON.parse(body);
-      response.end(`<h1>Hai ${name}!</h1>`);
-    });
+      request.on("end", () => {
+        body = Buffer.concat(body).toString();
+        const { name } = JSON.parse(body);
+        response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
+      });
+    } else {
+      response.end(
+        `<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`
+      );
+    }
+  } else {
+    response.end("<h1>Halaman tidak ditemukan!</h1>");
   }
 };
 
@@ -36,4 +50,6 @@ server.listen(port, host, () => {
 });
 
 // npm run start
-// curl -X POST -H "Content-Type: application/json" http://localhost:5000 -d "{\"name\": \"Ardhana\"}"
+// curl -X GET http://localhost:5000/about
+// curl -X POST -H "Content-Type: application/json" http://localhost:5000/about -d "{\"name\": \"Ahjoy\"}"
+// curl -X PUT http://localhost:5000/about
